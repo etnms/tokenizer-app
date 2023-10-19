84904c38e92dd0b0524e5479fc656b9d1dd6c532
etnms <etiennenemes@gmail.com>
2023-10-19 10:58:54 -0400

    improved backend function

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,9 @@
 import nltk
 from nltk.tokenize import word_tokenize
 from nltk import pos_tag
-from flask import Flask, request
+from nltk.stem import PorterStemmer
+from nltk.corpus import treebank
+from flask import Flask, jsonify, request
 from flask_cors import CORS, cross_origin
     #nltk.download('punkt') # for tokens
     #nltk.download('averaged_perceptron_tagger') # for tagged tokens
@@ -13,11 +15,17 @@ app.config['CORS_HEADERS'] = 'Content-Type'
 @app.route("/", methods=['POST'])
 
 def main():
-    data = request.form['text']
-    print(data)
-    sentence = data
+    data = request.json  # Access the JSON data sent from the frontend
+    sentence = data['text']
+    type = data['type']
     tokens = word_tokenize(sentence)
-    tagged_tokens = pos_tag(tokens)
-    return tagged_tokens
-    #return tagged_tokens
+    if (type == 'token'):
+        return tokens
+    if (type == 'tag'):
+        tagged_tokens = pos_tag(tokens)
+        return tagged_tokens
+    if (type == 'stem'):
+        stemmer = PorterStemmer()
+        stemmed_tokens = [stemmer.stem(word) for word in tokens]
+        return stemmed_tokens
 
